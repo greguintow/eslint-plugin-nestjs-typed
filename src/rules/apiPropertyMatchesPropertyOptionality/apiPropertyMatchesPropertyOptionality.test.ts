3fb27779ac0bc27df2d68f855cca57c0ae768dc3
Greg Teixeira <thunderdance6@gmail.com>
2022-02-06 00:55:41 -0300

    feat: make api property matches auto fixable

--- src/rules/apiPropertyMatchesPropertyOptionality/apiPropertyMatchesPropertyOptionality.test.ts
+++ src/rules/apiPropertyMatchesPropertyOptionality/apiPropertyMatchesPropertyOptionality.test.ts
@@ -44,8 +44,12 @@ ruleTester.run("api-property-matches-property-optionality", rule, {
     ],
     invalid: [
         {
+            name: "should replace ApiPropertyOptional with ApiProperty",
             code: `class TestClass {@Expose()
                 @ApiPropertyOptional()
+                thisIsAStringProp: string;}`,
+            output: `class TestClass {@Expose()
+                @ApiProperty()
                 thisIsAStringProp: string;}`,
             errors: [
                 {
@@ -54,18 +58,12 @@ ruleTester.run("api-property-matches-property-optionality", rule, {
             ],
         },
         {
-            code: `class TestClass {@Expose()
-                @ApiPropertyOptional()
-                thisIsAStringProp!: string;}`,
-            errors: [
-                {
-                    messageId: "shouldUseRequiredDecorator",
-                },
-            ],
-        },
-        {
+            name: "should replace ApiProperty with ApiPropertyOptional",
             code: `class TestClass {@Expose()
                 @ApiProperty()
+                thisIsAStringProp?: string;}`,
+            output: `class TestClass {@Expose()
+                @ApiPropertyOptional()
                 thisIsAStringProp?: string;}`,
             errors: [
                 {
@@ -74,8 +72,12 @@ ruleTester.run("api-property-matches-property-optionality", rule, {
             ],
         },
         {
+            name: "should replace ApiProperty with ApiPropertyOptional when is not marked with ? but is possibly undefined",
             code: `class TestClass {@Expose()
                 @ApiProperty()
+                thisIsAStringProp: string | undefined;}`,
+            output: `class TestClass {@Expose()
+                @ApiPropertyOptional()
                 thisIsAStringProp: string | undefined;}`,
             errors: [
                 {
@@ -84,62 +86,166 @@ ruleTester.run("api-property-matches-property-optionality", rule, {
             ],
         },
         {
+            name: "should add IsOptional decorator",
             code: `class TestClass {
                 @Field({ nullable: true })
                 @IsString()
                 thisIsAStringProp?: string;}`,
-            errors: [
-                {
-                    messageId: "shouldUseOptionalDecorator",
-                },
-            ],
-        },
-        {
-            code: `class TestClass {
-                @Field()
-                thisIsAStringProp?: string;}`,
-            errors: [
-                {
-                    messageId: "shouldUseOptionalDecorator",
-                },
-            ],
-        },
-        {
-            code: `class TestClass {
-                @Field()
+            output: `class TestClass {
+                @Field({ nullable: true })
+                @IsString()
                 @IsOptional()
                 thisIsAStringProp?: string;}`,
             errors: [
                 {
-                    messageId: "shouldUseOptionalDecorator",
+                    messageId: "shouldAddIsOptional",
                 },
             ],
         },
         {
+            name: "should add IsOptional decorator and Field as nullable true",
+            code: `class TestClass {
+                @Field()
+                @IsString()
+                thisIsAStringProp?: string;}`,
+            output: `class TestClass {
+                @Field({ nullable: true })
+                @IsString()
+                @IsOptional()
+                thisIsAStringProp?: string;}`,
+            errors: [
+                {
+                    messageId: "shouldSetFieldAsNullable",
+                },
+                {
+                    messageId: "shouldAddIsOptional",
+                },
+            ],
+        },
+        {
+            name: "should add nullable true argument with a field decorator with no args",
+            code: `class TestClass {
+                @Field()
+                thisIsAStringProp?: string;}`,
+            errors: [
+                {
+                    messageId: "shouldSetFieldAsNullable",
+                },
+            ],
+            output: `class TestClass {
+                @Field({ nullable: true })
+                thisIsAStringProp?: string;}`,
+        },
+        {
+            name: "should add nullable true argument with a field decorator when is not marked with ? but is possibly null",
+            code: `class TestClass {
+                @Field()
+                thisIsAStringProp: string | null;}`,
+            errors: [
+                {
+                    messageId: "shouldSetFieldAsNullable",
+                },
+            ],
+            output: `class TestClass {
+                @Field({ nullable: true })
+                thisIsAStringProp: string | null;}`,
+        },
+        {
+            name: "should add nullable true argument with a field decorator with no args and with IsOptional decorator",
+            code: `class TestClass {
+                @Field()
+                @IsOptional()
+                thisIsAStringProp?: string;}`,
+            output: `class TestClass {
+                @Field({ nullable: true })
+                @IsOptional()
+                thisIsAStringProp?: string;}`,
+            errors: [
+                {
+                    messageId: "shouldSetFieldAsNullable",
+                },
+            ],
+        },
+        {
+            name: "should add nullable true with a field decorator with one arg of options",
+            code: `class TestClass {
+                @Field({ name: 'test' })
+                thisIsAStringProp?: string;}`,
+            output: `class TestClass {
+                @Field({ name: 'test', nullable: true })
+                thisIsAStringProp?: string;}`,
+            errors: [
+                {
+                    messageId: "shouldSetFieldAsNullable",
+                },
+            ],
+        },
+        {
+            name: "should add nullable true with a field decorator with first arg of a type",
+            code: `class TestClass {
+                @Field(() => foo)
+                thisIsAStringProp?: string;}`,
+            output: `class TestClass {
+                @Field(() => foo, { nullable: true })
+                thisIsAStringProp?: string;}`,
+            errors: [
+                {
+                    messageId: "shouldSetFieldAsNullable",
+                },
+            ],
+        },
+        {
+            name: "should add nullable true with a field decorator with 2 args",
+            code: `class TestClass {
+                @Field(() => foo, { name: 'test' })
+                thisIsAStringProp?: string;}`,
+            output: `class TestClass {
+                @Field(() => foo, { name: 'test', nullable: true })
+                thisIsAStringProp?: string;}`,
+            errors: [
+                {
+                    messageId: "shouldSetFieldAsNullable",
+                },
+            ],
+        },
+        {
+            name: "should remove nullable true argument and isOptional decorator",
             code: `class TestClass {
                 @Field({ nullable: true })
                 @IsOptional()
                 thisIsAStringProp: string;}`,
-            errors: [
-                {
-                    messageId: "shouldUseRequiredDecorator",
-                },
-            ],
-        },
-        {
-            code: `class TestClass {
+            output: `class TestClass {
                 @Field()
-                @IsOptional()
                 thisIsAStringProp: string;}`,
             errors: [
                 {
+                    messageId: "shouldRemoveNullableFromField",
+                },
+                {
                     messageId: "shouldUseRequiredDecorator",
                 },
             ],
         },
         {
+            name: "should remove nullable true property",
+            code: `class TestClass {
+                @Field({ name: 'test', nullable: true })
+                thisIsAStringProp: string;}`,
+            output: `class TestClass {
+                @Field({ name: 'test' })
+                thisIsAStringProp: string;}`,
+            errors: [
+                {
+                    messageId: "shouldRemoveNullableFromField",
+                },
+            ],
+        },
+        {
+            name: "should remove isOptional decorator",
             code: `class TestClass {
                 @IsOptional()
+                thisIsAStringProp: string;}`,
+            output: `class TestClass {
                 thisIsAStringProp: string;}`,
             errors: [
                 {
--- src/rules/apiPropertyMatchesPropertyOptionality/apiPropertyMatchesPropertyOptionality.ts
+++ src/rules/apiPropertyMatchesPropertyOptionality/apiPropertyMatchesPropertyOptionality.ts
@@ -3,7 +3,19 @@ import {AST_NODE_TYPES} from "@typescript-eslint/experimental-utils";
 import {createRule} from "../../utils/createRule";
 import {typedTokenHelpers} from "../../utils/typedTokenHelpers";
 import * as classValidator from "class-validator";
-import {RuleContext} from "@typescript-eslint/experimental-utils/dist/ts-eslint";
+import {
+    RuleContext,
+    RuleFix,
+} from "@typescript-eslint/experimental-utils/dist/ts-eslint";
+import {appendArgument} from "../../utils/appendArgument";
+import {replaceArgument} from "../../utils/replaceArgument";
+import {
+    getPropertiesOfObjectWithNoBrackets,
+    stripObject,
+} from "../../utils/parentheses";
+import {renameDecorator} from "../../utils/renameDecorator";
+import {appendDecorator} from "../../utils/appendDecorator";
+import {removeDecorator} from "../../utils/removeDecorator";
 
 const CLASS_VALIDATOR_DECORATOR_NAMES = new Set(
     Object.keys(classValidator as object)
@@ -11,93 +23,210 @@ const CLASS_VALIDATOR_DECORATOR_NAMES = new Set(
 
 CLASS_VALIDATOR_DECORATOR_NAMES.delete("IsOptional");
 
-export const shouldUseRequiredDecorator = (
-    node: TSESTree.PropertyDefinition
-): boolean => {
-    const hasOptionalDecorator = typedTokenHelpers.nodeHasDecoratorsNamed(
-        node,
-        ["ApiPropertyOptional", "IsOptional"]
-    );
+type Context = Readonly<
+    RuleContext<
+        | "shouldUseOptionalDecorator"
+        | "shouldUseRequiredDecorator"
+        | "shouldAddIsOptional"
+        | "shouldRemoveNullableFromField"
+        | "shouldSetFieldAsNullable",
+        never[]
+    >
+>;
 
+export const shouldUseRequiredDecorator = (
+    node: TSESTree.PropertyDefinition,
+    context: Context
+) => {
+    const sourceCode = context.getSourceCode();
     const isOptionalPropertyValue =
         typedTokenHelpers.isOptionalPropertyValue(node);
 
-    if (!isOptionalPropertyValue) {
-        const [field] = typedTokenHelpers.getDecoratorsNamedOrdered(node, [
+    if (isOptionalPropertyValue) return;
+
+    const [field, apiPropertyOptional, isOptionalDecorator] =
+        typedTokenHelpers.getDecoratorsNamedOrdered(node, [
             "Field",
+            "ApiPropertyOptional",
+            "IsOptional",
         ]);
-        if (field) {
-            const fieldArguments = (field.expression as TSESTree.CallExpression)
-                .arguments as TSESTree.ObjectExpression[];
-            const isOptionalValue = fieldArguments.some((argument) =>
+    if (field) {
+        const fieldArgument = (
+            field.expression as TSESTree.CallExpression
+        ).arguments.find(
+            (argument) => argument.type === AST_NODE_TYPES.ObjectExpression
+        ) as TSESTree.ObjectExpression | undefined;
+        if (fieldArgument) {
+            const nullableProperty = typedTokenHelpers.getPropertyWithValue(
+                fieldArgument,
+                "nullable",
+                true
+            );
+            if (nullableProperty) {
+                context.report({
+                    messageId: "shouldRemoveNullableFromField",
+                    node: field,
+                    fix(fixer) {
+                        if (fieldArgument.properties.length === 1) {
+                            return fixer.removeRange(fieldArgument.range);
+                        }
+                        const argumentText = sourceCode.getText(fieldArgument);
+                        const text = sourceCode.getText(nullableProperty);
+                        const newArgumentText = argumentText.replace(text, "");
+                        return replaceArgument(
+                            fixer,
+                            fieldArgument,
+                            `{ ${stripObject(newArgumentText)} }`,
+                            sourceCode
+                        );
+                    },
+                });
+            }
+        }
+    }
+    if (apiPropertyOptional || isOptionalDecorator) {
+        context.report({
+            messageId: "shouldUseRequiredDecorator",
+            node,
+            fix(fixer) {
+                return [
+                    apiPropertyOptional
+                        ? renameDecorator(
+                              fixer,
+                              apiPropertyOptional,
+                              "ApiProperty"
+                          )
+                        : undefined,
+                    isOptionalDecorator
+                        ? removeDecorator(fixer, isOptionalDecorator)
+                        : undefined,
+                ].filter(Boolean) as readonly RuleFix[];
+            },
+        });
+    }
+};
+
+export function shouldUseOptionalDecorator(
+    node: TSESTree.PropertyDefinition,
+    context: Context
+) {
+    const sourceCode = context.getSourceCode();
+    const isOptionalPropertyValue =
+        typedTokenHelpers.isOptionalPropertyValue(node);
+
+    if (!isOptionalPropertyValue) return;
+    const [field, isOptional, apiProperty] =
+        typedTokenHelpers.getDecoratorsNamedOrdered(node, [
+            "Field",
+            "IsOptional",
+            "ApiProperty",
+        ]);
+    let isSetAsRequired = false;
+    if (field) {
+        const fieldArguments = (field.expression as TSESTree.CallExpression)
+            .arguments as TSESTree.ObjectExpression[];
+        let hasNullable = false;
+        isSetAsRequired =
+            !fieldArguments.some((argument) =>
                 typedTokenHelpers.getPropertyValueEqualsExpected(
                     argument,
                     "nullable",
                     true
                 )
-            );
-            if (isOptionalValue) return true;
-        }
-    }
-
-    return hasOptionalDecorator && !isOptionalPropertyValue;
-};
-
-export const shouldUseOptionalDecorator = (
-    node: TSESTree.PropertyDefinition,
-    context: Readonly<
-        RuleContext<
-            | "shouldUseOptionalDecorator"
-            | "shouldUseRequiredDecorator"
-            | "shouldSetFieldAsNullable",
-            never[]
-        >
-    >
-): boolean => {
-    const hasRequiredDecorator = typedTokenHelpers.nodeHasDecoratorsNamed(
-        node,
-        ["ApiProperty"]
-    );
-
-    const isOptionalPropertyValue =
-        typedTokenHelpers.isOptionalPropertyValue(node);
-    if (isOptionalPropertyValue) {
-        const [field, isOptional] = typedTokenHelpers.getDecoratorsNamedOrdered(
-            node,
-            ["Field", "IsOptional"]
-        );
-        let isSetAsRequired = false;
-        if (field) {
-            const fieldArguments = (field.expression as TSESTree.CallExpression)
-                .arguments as TSESTree.ObjectExpression[];
-            isSetAsRequired =
-                !fieldArguments.some((argument) =>
-                    typedTokenHelpers.getPropertyValueEqualsExpected(
-                        argument,
-                        "nullable",
-                        true
-                    )
-                ) ||
-                fieldArguments.some((argument) =>
+            ) ||
+            fieldArguments.some((argument) => {
+                if (
                     typedTokenHelpers.getPropertyValueEqualsExpected(
                         argument,
                         "nullable",
                         false
                     )
-                );
-            if (isSetAsRequired) {
-                context.report({
-                    node,
-                    messageId: "shouldSetFieldAsNullable",
-                    fix: (fixer) => {
-                        if (fieldArguments.length === 2) {
-                            // fixer.replaceText(fieldArguments[1], );
+                ) {
+                    hasNullable = true;
+                    return true;
+                }
+                return false;
+            });
+        if (isSetAsRequired) {
+            context.report({
+                node: field,
+                messageId: "shouldSetFieldAsNullable",
+                fix(fixer) {
+                    if (fieldArguments.length > 0 && hasNullable) {
+                        const foundPropertyOfName = fieldArguments
+                            .flatMap((argument) => argument.properties)
+                            .find(
+                                (p) =>
+                                    (
+                                        (p as TSESTree.Property)
+                                            .key as TSESTree.Identifier
+                                    ).name === "nullable"
+                            ) as TSESTree.Property;
+                        if (foundPropertyOfName) {
+                            return fixer.replaceText(
+                                foundPropertyOfName.value,
+                                "true"
+                            );
                         }
-                    },
-                });
-            }
+                    } else {
+                        switch (fieldArguments.length) {
+                            case 0: {
+                                return appendArgument(
+                                    fixer,
+                                    field.expression as TSESTree.CallExpression,
+                                    `{ nullable: true }`,
+                                    sourceCode
+                                );
+                            }
+                            case 2: {
+                                const secondArgumentText =
+                                    getPropertiesOfObjectWithNoBrackets(
+                                        fieldArguments[1],
+                                        sourceCode
+                                    );
+
+                                return replaceArgument(
+                                    fixer,
+                                    fieldArguments[1],
+                                    `{ ${secondArgumentText}, nullable: true }`,
+                                    sourceCode
+                                );
+                            }
+                            case 1: {
+                                if (
+                                    fieldArguments[0].type !==
+                                    AST_NODE_TYPES.ObjectExpression
+                                ) {
+                                    return appendArgument(
+                                        fixer,
+                                        field.expression as TSESTree.CallExpression,
+                                        `{ nullable: true }`,
+                                        sourceCode
+                                    );
+                                }
+                                const firstArgumentText =
+                                    getPropertiesOfObjectWithNoBrackets(
+                                        fieldArguments[0],
+                                        sourceCode
+                                    );
+
+                                return replaceArgument(
+                                    fixer,
+                                    fieldArguments[0],
+                                    `{ ${firstArgumentText}, nullable: true }`,
+                                    sourceCode
+                                );
+                            }
+                        }
+                    }
+                    return null;
+                },
+            });
         }
-        const hasDecorator = node.decorators?.some(
+    }
+    const shouldAddIsOptional =
+        !isOptional &&
+        node.decorators?.some(
             (decorator) =>
                 decorator.expression.type === AST_NODE_TYPES.CallExpression &&
                 decorator.expression.callee.type ===
@@ -106,15 +235,30 @@ export const shouldUseOptionalDecorator = (
                     decorator.expression.callee.name
                 )
         );
-        if (!isOptional && hasDecorator) {
-            isSetAsRequired = true;
-        }
 
-        if (isSetAsRequired) return true;
+    if (shouldAddIsOptional) {
+        context.report({
+            node,
+            messageId: "shouldAddIsOptional",
+            fix(fixer) {
+                return appendDecorator(fixer, node, "@IsOptional()");
+            },
+        });
     }
-
-    return hasRequiredDecorator && isOptionalPropertyValue;
-};
+    if (apiProperty) {
+        context.report({
+            node: apiProperty,
+            messageId: "shouldUseOptionalDecorator",
+            fix(fixer) {
+                return renameDecorator(
+                    fixer,
+                    apiProperty,
+                    "ApiPropertyOptional"
+                );
+            },
+        });
+    }
+}
 
 const rule = createRule({
     name: "api-property-matches-property-optionality",
@@ -125,12 +269,15 @@ const rule = createRule({
             requiresTypeChecking: false,
         },
         messages: {
-            shouldUseOptionalDecorator: `Property marked as optional should use @IsOptional decorator and Field as nullable true`,
+            shouldUseOptionalDecorator: `Property marked as optional should use @ApiPropertyOptional decorator`,
             shouldSetFieldAsNullable: `Property marked as optional should use @Field as nullable true`,
+            shouldAddIsOptional: `Property marked as optional should add @IsOptional when there are other class validator decorators`,
             shouldUseRequiredDecorator: `Property marked as required should not use nullable decorators`,
+            shouldRemoveNullableFromField: `Property marked as required should not use @Field with nullable true`,
         },
         schema: [],
         hasSuggestions: false,
+        fixable: "code",
         type: "suggestion",
     },
     defaultOptions: [],
@@ -139,21 +286,8 @@ const rule = createRule({
         return {
             // eslint-disable-next-line @typescript-eslint/naming-convention
             PropertyDefinition(node: TSESTree.PropertyDefinition): void {
-                if (shouldUseOptionalDecorator(node)) {
-                    context.report({
-                        node,
-                        messageId: "shouldUseOptionalDecorator",
-                        fix: (fixer) => {
-                            fixer.insertTextAfter();
-                        },
-                    });
-                }
-                if (shouldUseRequiredDecorator(node)) {
-                    context.report({
-                        node,
-                        messageId: "shouldUseRequiredDecorator",
-                    });
-                }
+                shouldUseOptionalDecorator(node, context);
+                shouldUseRequiredDecorator(node, context);
             },
         };
     },
